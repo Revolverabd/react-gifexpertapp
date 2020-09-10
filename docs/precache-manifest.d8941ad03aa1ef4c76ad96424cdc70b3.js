2f307cf9ec919865fba191bfbb705ef2de4f6c02
pedro <pedro.xp.ns@gmail.com>
2020-09-10 15:08:22 -0300

    Cambio de nombre a carpeta build por docs para despliegue en github

---
+++ docs/precache-manifest.d8941ad03aa1ef4c76ad96424cdc70b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a87cbcdd41408bb142038dd234fa8de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "080b49a3b3e149ea1192",
+    "url": "/static/css/main.0206168b.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080b49a3b3e149ea1192",
+    "url": "/static/js/main.35317965.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
